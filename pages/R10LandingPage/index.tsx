2bf013ff0ef4650a72ce0642823bbfb7aeee0a3c
Aswin CJ <aswincj@riafy.me>
2023-06-07 04:38:19 +0530

    Initial commit + Added Landing Header

---
+++ pages/R10LandingPage/index.tsx
@@ -0,0 +1,25 @@
+import Layout from "@/components/Layout";
+import Main from "./Main";
+import Hot from "./Hot";
+import Dream from "./Dream";
+import Collections from "./Collections";
+import Auctions from "@/components/Auctions";
+import Spotlight from "./Spotlight";
+import Creativity from "./Creativity";
+import Newsletter from "@/components/Newsletter";
+
+import { auctions } from "@/mocks/auctions";
+
+const HomePage = () => {
+    return (
+        <Layout layoutNoOverflow noRegistration>
+            <Dream />
+            <Main />
+            <Hot />
+            <Creativity />
+            <Newsletter />
+        </Layout>
+    );
+};
+
+export default HomePage;
---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+import type { NextPage } from "next";
+import HomePage from "@/templates/HomePage";
+import R10LandingPage from "pages/R10LandingPage";
+
+const Home: NextPage = () => {
+    return <R10LandingPage />;
+};
+
+export default Home;
